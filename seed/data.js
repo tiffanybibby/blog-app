d82b9aa8eef01e489c236678458899b1ce64bdc8
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-09-03 14:44:16 -0400

    setup express app & built full CRUD functionality

---
+++ seed/data.js
@@ -0,0 +1,31 @@
+import db from '../db/connection.js'
+import Post from '../models/post.js'
+
+const insertData = async () => {
+  // reset database
+  await db.dropDatabase()
+
+  const posts =
+    [
+      {
+        "title": "Teamwork makes the dream work",
+        "imgURL": "https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "author": "Tiffany, Nia-Emani, Rachel, & Daria"
+      },
+      {
+        "title": "There's no i team",
+        "imgURL": "https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "author": "Tiffany, Nia-Emani, Rachel, & Daria"
+      }
+    ]
+
+  await Post.insertMany(posts)
+  console.log("Created posts!")
+
+  // close database connection. done.
+  db.close()
+}
+
+insertData()
