4981cf75d346b3ec8379944c7bb3ce395585c990
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-09-03 14:02:43 -0400

    setup mongoose library & Schema

---
+++ models/post.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const Post = new Schema(
+  {
+    title: { type: String, required: true },
+    imgURL: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('posts', Post)
