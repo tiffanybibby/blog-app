15be7ae818ef41d8263cb92189eb98c8a0f79210
Daria Krestina <dariakrestina@Darias-MacBook-Pro.local>
2021-09-08 15:03:18 -0400

    Working on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Posts from "./screens/Posts/Posts";
+import CreatePost from "./screens/CreatePost/CreatePost";
+import EditPost from "./screens/EditPost/EditPost";
+import PostDetail from "./screens/PostDetail/PostDetail";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Posts} />
+        <Route path="/create-post" component={CreatePost} />
+        <Route exact path="/posts/:id/edit" component={EditPost} />
+        <Route exact path="/posts/:id" component={PostDetail} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/screens/Posts/Posts.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import './Posts.css'
+
+import Post from '../../components/Post/Post'
+import Layout from '../../components/Layout/Layout'
+import { getPosts } from '../../services/posts'
+
+const Posts = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+      const fetchPosts = async () => {
+      const allPosts = await getPosts()
+      setPosts(allPosts)
+    }
+    fetchPosts()
+  }, [])
+
+  return (
+    <Layout>
+      <div className='posts'>
+        {posts.map((post, index) => {
+          return (
+            <Post
+              _id={post._id}
+              title={post.title}
+              imgURL={post.imgURL}
+              content={post.content}
+              author={post.author}
+              key={index}
+            />
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export default Posts
